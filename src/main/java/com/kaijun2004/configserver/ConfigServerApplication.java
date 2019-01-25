9c0b426893104a3d57c4d5e508135a7570a91c9b
kaijun.yuan <17761291283@126.com>
2019-01-25 16:56:43 +0800

    配置修改

--- src/main/java/com/kaijun2004/configserver/ConfigServerApplication.java
+++ src/main/java/com/kaijun2004/configserver/ConfigServerApplication.java
@@ -1,11 +1,14 @@
 package com.kaijun2004.configserver;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
-@SpringCloudApplication
+@SpringBootApplication
 @EnableConfigServer
+@EnableDiscoveryClient
+
 public class ConfigServerApplication {
 
     public static void main(String[] args) {
